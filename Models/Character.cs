831cdbdb2d57360839f84e32291302f04e504334
Urbano Coutinho Neto <urbanoneto37@gmail.com>
2026-05-28 18:51:05 -0300

    Add files via upload

---
+++ Models/Character.cs
@@ -0,0 +1,10 @@
+namespace BlazorDbzApiPersonaExtra.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string Image { get; set; }
+    }
+}
